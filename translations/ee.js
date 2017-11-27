f2f6f3a75de4d6e6717c6249902ff21f265dbcab
martin donadieu <martindonadieu@gmail.com>
2017-11-27 15:26:33 +0100

    split lang in differents files

---
+++ translations/ee.js
@@ -0,0 +1,90 @@
+var ee = {
+    common: {
+      company_name: 'Digital shift',
+      signup_sentence: 'Pret a commencer ? Souscris maintenant!',
+      signup: 'Souscrire'
+    },
+    header: {
+    },
+    value_proposition: {
+      first: 'Votre MVP, en 4 semaines.',
+      second: 'Ni plus, ni moins.'
+    },
+    grid: {
+      '1': {
+        title: 'Fully Responsive',
+        subtitle: 'This theme will look great on any device, no matter the size!'
+      },
+      '2': {
+        title: 'Future Ready',
+        subtitle: 'Featuring the latest Tech stack framework!'
+      },
+      '3': {
+        title: 'Easy to Use',
+        subtitle: 'Ready to use, simple to customize for your team!'
+      },
+    },
+    showcase: {
+      '1': {
+        title: 'IDEATION',
+        subtitle: 'Provoquer le succès',
+        content: 'We believe that every idea has a potential. Success is reachable by everyone but you need to start somewhere. Please, use the chat or the contact form to tell us more about your project. In this way, we will be able to get back to you quickly with a tailor-made proposition including an estimate. Of course, at this step, a non-disclosure agreement (NDA) can be signed if needed.'
+      },
+      '2': {
+        title: 'SCOPING',
+        subtitle: 'Mettre le contact',
+        content: 'Here you go. Before reaching the sky, we need to be fully aligned with your vision and objectives. Let\'s meet, grab a coffee, sit somewhere and discuss it. This is a big moment in your MVP life.\bDuring this meeting, we will go through every aspects of your project so as to match your expectations and our expertise. It will give us solid basics to start building the MVP you need. Concretely, we will measure the feasibility, define the requirements, plan the next steps and give you a clear view on the deliverables.'
+      },
+      '3': {
+        title: 'USER EXPERIENCE',
+        subtitle: 'Ajouter de la magie',
+        content: 'Now it\'s time to create a unique experience for your future users. User experience is essential to design a high value solution. It will ensure that your product matches with users\' expectations. To do so, we will use specific UX design tools and our knowledge of the latest trends and good practices to conceive the best user experience for your MVP. At this point, we will produce a fully functional prototype giving you a valuable overview of your product.'
+      },
+      '4': {
+        title: 'VISUAL DESIGN',
+        subtitle: 'Propulser jusqu\'au ciel',
+        content: 'Now we\'ve built the skeleton, let\'s add the skin. Once you\'ve approved the prototype, our User Interface Designers will brainstorm and create several visual designs that fit both your vision and product value (don\'t worry we won\'t use too much pink, stars and unicorns). Based on your preferences, we will focus on one specific design and make few iterations if it\'s necessary. At the end of this stage, we will deliver a visual design kit ready to be implemented.'
+      },
+      '5': {
+        title: 'DEVELOPMENT',
+        subtitle: 'Forger une solution viable',
+        content: 'This step involves building your MVP. By selecting the technology that best suits the needs of your project, the end-product will be easily extendable and scalable. We always keep the development process as transparent as possible, which means that you will be able to follow its progress.'
+      },
+      '6': {
+        title: 'DELIVERY',
+        subtitle: 'Prêt à conquérir le ciel',
+        content: 'At this point, your MVP is ready. We will produce the deliverable the way you want it by making it live and handing the project to you. During this phase, we will test the product several times making sure it is stable and sustainable.'
+      },
+      '7': {
+        title: 'VIDEO PRODUCTION',
+        subtitle: 'Maximiser l\'impact',
+        content: 'This is an additional service that includes the preparation of a script, storyboards, voice-over, video production and post-production, and starts just after the discovery phase. It is carried out simultaneously with the other steps.'
+      }
+    },
+    testimonial: {
+      header: '',
+      '1': {
+        title: '',
+        content: ''
+      },
+      '2': {
+        title: '',
+        content: ''
+      },
+      '3': {
+        title: '',
+        content: ''
+      }
+    },
+    footer: {
+      about: 'About',
+      jobs: 'Jobs',
+      contact: 'Contact',
+      terms: 'Terms of Use',
+      privacy: 'Privacy Policy'
+    }
+  }
+
+module.exports = lang;if (!window.landing)
+    window.landing = {}
+window.landing.ee = ee;
---
+++ translations/en.js
@@ -0,0 +1,90 @@
+const en = {
+        common: {
+          company_name: 'Digital shift',
+          signup_sentence: 'Ready to get started? Sign up now!',
+          signup: 'Signup'
+        },
+        header: {
+        },
+        value_proposition: {
+          first: 'Votre MVP, in 4 weeks.',
+          second: 'Nothing more, nothing less.'
+        },
+        grid: {
+          '1': {
+            title: 'Fully Responsive',
+            subtitle: 'This theme will look great on any device, no matter the size!'
+          },
+          '2': {
+            title: 'Future Ready',
+            subtitle: 'Featuring the latest Tech stack framework!'
+          },
+          '3': {
+            title: 'Easy to Use',
+            subtitle: 'Ready to use, simple to customize for your team!'
+          },
+        },
+        showcase: {
+          '1': {
+            title: 'IDEATION',
+            subtitle: 'Initiate success',
+            content: 'We believe that every idea has a potential. Success is reachable by everyone but you need to start somewhere. Please, use the chat or the contact form to tell us more about your project. In this way, we will be able to get back to you quickly with a tailor-made proposition including an estimate. Of course, at this step, a non-disclosure agreement (NDA) can be signed if needed.'
+          },
+          '2': {
+            title: 'SCOPING',
+            subtitle: 'Turn on the power',
+            content: 'Here you go. Before reaching the sky, we need to be fully aligned with your vision and objectives. Let\'s meet, grab a coffee, sit somewhere and discuss it. This is a big moment in your MVP life.\bDuring this meeting, we will go through every aspects of your project so as to match your expectations and our expertise. It will give us solid basics to start building the MVP you need. Concretely, we will measure the feasibility, define the requirements, plan the next steps and give you a clear view on the deliverables.'
+          },
+          '3': {
+            title: 'USER EXPERIENCE',
+            subtitle: 'Add magic to the project',
+            content: 'Now it\'s time to create a unique experience for your future users. User experience is essential to design a high value solution. It will ensure that your product matches with users\' expectations. To do so, we will use specific UX design tools and our knowledge of the latest trends and good practices to conceive the best user experience for your MVP. At this point, we will produce a fully functional prototype giving you a valuable overview of your product.'
+          },
+          '4': {
+            title: 'VISUAL DESIGN',
+            subtitle: 'Propel the project to the sky',
+            content: 'Now we\'ve built the skeleton, let\'s add the skin. Once you\'ve approved the prototype, our User Interface Designers will brainstorm and create several visual designs that fit both your vision and product value (don\'t worry we won\'t use too much pink, stars and unicorns). Based on your preferences, we will focus on one specific design and make few iterations if it\'s necessary. At the end of this stage, we will deliver a visual design kit ready to be implemented.'
+          },
+          '5': {
+            title: 'DEVELOPMENT',
+            subtitle: 'Forge a strong solution',
+            content: 'This step involves building your MVP. By selecting the technology that best suits the needs of your project, the end-product will be easily extendable and scalable. We always keep the development process as transparent as possible, which means that you will be able to follow its progress.'
+          },
+          '6': {
+            title: 'DELIVERY',
+            subtitle: 'All set to conquest the sky',
+            content: 'At this point, your MVP is ready. We will produce the deliverable the way you want it by making it live and handing the project to you. During this phase, we will test the product several times making sure it is stable and sustainable.'
+          },
+          '7': {
+            title: 'VIDEO PRODUCTION',
+            subtitle: 'Boost your impact',
+            content: 'This is an additional service that includes the preparation of a script, storyboards, voice-over, video production and post-production, and starts just after the discovery phase. It is carried out simultaneously with the other steps.'
+          }
+        },
+        testimonial: {
+          header: '',
+          '1': {
+            title: '',
+            content: ''
+          },
+          '2': {
+            title: '',
+            content: ''
+          },
+          '3': {
+            title: '',
+            content: ''
+          }
+        },
+        footer: {
+          about: 'About',
+          jobs: 'Jobs',
+          contact: 'Contact',
+          terms: 'Terms of Use',
+          privacy: 'Privacy Policy'
+        }
+      }
+
+if (!window.landing)
+    window.landing = {}
+window.landing.en = en;
