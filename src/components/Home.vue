7748848d0f9dcb28d27954b2360120fb0776c42d
martin donadieu <martindonadieu@gmail.com>
2018-01-13 19:28:31 +0100

    fix error in beta site

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -24,47 +24,6 @@
             </div>
           </header>
 
-          <!-- Icons Grid -->
-          <section class="features-icons bg-forgr text-center">
-            <!-- <div class="container">
-              <div class="row">
-                <div class="col-lg-4">
-                  <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
-                    <div class="features-icons-icon d-flex">
-                      <i class="icon-screen-smartphone m-auto text-primary"></i>
-                    </div>
-                    <h3>{{ $t("grid.1.title") }}</h3>
-                    <p class="lead mb-0">
-                        {{ $t("grid.1.subtitle") }}
-                  </p>
-                  </div>
-                </div>
-                <div class="col-lg-4">
-                  <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
-                    <div class="features-icons-icon d-flex">
-                      <i class="icon-rocket m-auto text-primary"></i>
-                    </div>
-                    <h3>{{ $t("grid.2.title") }}</h3>
-                    <p class="lead mb-0">
-                        {{ $t("grid.2.subtitle") }}
-                    </p>
-                  </div>
-                </div>
-                <div class="col-lg-4">
-                  <div class="features-icons-item mx-auto mb-0 mb-lg-3">
-                    <div class="features-icons-icon d-flex">
-                      <i class="icon-mustache m-auto text-primary"></i>
-                    </div>
-                    <h3>{{ $t("grid.3.title") }}</h3>
-                    <p class="lead mb-0">
-                        {{ $t("grid.3.subtitle") }}
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div> -->
-          </section>
-
           <!-- Image Showcases -->
           <section class="showcase">
             <div class="container-fluid p-0">
