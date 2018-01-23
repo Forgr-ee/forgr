f7d4b288d6caa280733e58b4ba643985f587d92f
martin donadieu <martindonadieu@gmail.com>
2018-01-23 22:13:20 +0100

    fix lang + prod beta

--- src/lang/fr.js
+++ src/lang/fr.js
@@ -43,32 +43,32 @@ const fr = {
     2: {
       title: 'CIBLAGE',
       subtitle: 'Mettre le contact',
-      content: 'Avant d\'atteindre les étoiles, nous devons être entièrement aligné avec votre vision et vos objectifs.\n Alors remplissez le formulaire. Ensemble, nous allons parcourir tous les aspects de votre projet afin de s\'assurer que vos attentes et enjeux sont bien défini.\n Concrètement, nous allons mesurer la faisabilité, définir les besoins, etablir un devis, et vous donner une vision claire sur les prochaines étapes et les livrables.',
+      content: 'Avant d\'atteindre les étoiles, nous devons être entièrement alignés avec votre vision et vos objectifs. Pour ça, remplissez le formulaire. Ensemble, nous allons parcourir tous les aspects de votre projet afin de s\'assurer que vos attentes et enjeux sont bien définis. Concrètement, nous allons mesurer la faisabilité, définir les besoins, établir un devis et vous donner une vision claire sur les prochaines étapes et les livrables.'
     },
     3: {
       title: 'EXPERIENCE UTILISATEUR',
       subtitle: 'Ajouter de la magie',
-      content: 'Au delà de l\'esthétisme, cela garantira que votre produit correspond aux attentes des utilisateurs. Pour ce faire, nous utiliserons notre connaissance des dernières tendances et bonnes pratiques pour concevoir la meilleure expérience utilisateur pour votre MVP. À ce stade, nous produirons un prototype entièrement fonctionnel vous donnant un aperçu de votre produit.',
+      content: 'Au delà de l\'esthétisme, elle vous garantira un produit qui correspond aux attentes des utilisateurs. Pour ce faire, nous utiliserons notre connaissance des dernières tendances et bonnes pratiques pour concevoir la meilleure expérience utilisateur pour votre MVP. À ce stade, nous produirons un prototype entièrement fonctionnel vous donnant un aperçu de votre produit.'
     },
     4: {
       title: 'ASPECT VISUEL',
       subtitle: 'Propulser jusqu\'au ciel',
-      content: 'Une fois que vous avez approuvé le prototype, nos concepteurs d\'interface utilisateur réfléchissent et créent plusieurs conceptions visuelles qui correspondent à votre vision et à la valeur de votre produit (ne vous inquiétez pas, nous n\'utiliserons pas trop de roses, étoiles et licornes). À la fin de cette étape, nous livrerons un kit de conception visuelle prêt à être mis en œuvre.',
+      content: 'Une fois que vous avez approuvé le prototype, nos concepteurs d\'interface utilisateur réfléchissent et créent plusieurs conceptions visuelles qui correspondent à votre vision et à la valeur de votre produit (ne vous inquiétez pas, nous n’abuserons ni des roses, étoiles et/ou licornes). À la fin de cette étape, nous livrerons un kit de conception visuelle prêt à être mis en œuvre.'
     },
     5: {
       title: 'DEVELOPPEMENT',
       subtitle: 'Forger une solution viable',
-      content: 'Nous croyons fortement que certaines technologies sont plus adaptées pour la construction d\'un MVP, c\'est pour cela que nous nous sommes spécialisés dans celles-ci et qu\'un rendu en 4 semaines devient possible, le produit final sera robuste et évolutif. Nous gardons toujours le processus de développement ainsi vous serez en mesure de suivre ses progrès.',
+      content: 'Nous croyons fortement que certaines technologies sont plus adaptées que d’autres lors de la construction d\'un MVP. C\'est pourquoi en nous spécialisant sur ces dernières et qu’un rendu en 4 semaines devient possible. Le produit final sera robuste et évolutif. Nous gardons toujours le processus de développement transparent, ainsi vous serez en mesure de suivre son progrès.'
     },
     6: {
       title: 'LIVRAISON',
       subtitle: 'Prêt à conquérir le ciel',
-      content: 'À ce stade, votre MVP est prêt ! Après avoir finalisé le livrable, nous nous assurerons qu\'il arrive en bonne et due forme entre vos mains. Pour ce faire, nous réaliserons une série de tests pour constater et valider la qualité de votre MVP. ',
+      content: 'À ce stade, votre MVP est prêt ! Après avoir finalisé le livrable, nous nous assurerons qu\'il arrive en bonne et due forme entre vos mains. Pour ce faire, nous réaliserons une série de tests pour constater et valider la qualité de votre MVP.'
     },
     7: {
       title: 'PRODUCTION VIDEO',
       subtitle: 'Maximiser l\'impact',
-      content: 'Aujourd\'hui, les vidéos sont partout (même qu\'elles noient ton fil d\'actualité Facebook). Elles sont considérées comme un média riche de par leur excellent impact sur les individus. Nous sommes profondément attachés à elles car nous sommes convaincus qu\'elles apporteront une forte valeur ajoutée à votre MVP. Convaincu ? Discutons-en !',
+      content: 'Aujourd\'hui, les vidéos sont partout (même qu\'elles noient votre fil d\'actualité Facebook). Elles sont considérées comme un média riche de par leur excellent impact sur les individus. Nous sommes profondément attachés à elles car nous sommes convaincus qu\'elles apporteront une forte valeur ajoutée à votre MVP. Convaincu ? Discutons-en !'
     },
   },
   testimonial: {
