29dbab76e5341c43477feac37ef3ab1809fcab28
martin donadieu <martindonadieu@gmail.com>
2018-01-29 15:39:28 +0100

    fix CTA size

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -14,7 +14,7 @@
                 <div class="col-md-10 col-lg-8 col-xl-7 mx-auto">
                   <form>
                     <div class="form-row center-content">
-                      <div class="col-12 col-md-3">
+                      <div class="col-12 col-md-4">
                           <a class="typeform-share button btn btn-lg btn-primary animated-button victoria-four" :href="typeform_url" data-mode="popup" data-hide-headers=true data-hide-footer=true target="_blank" style="z-index: 1;">{{ $t("common.signup") }}</a>
                       </div>
                     </div>
@@ -205,13 +205,15 @@ export default {
   },
   computed: {
     typeform_url() {
-      const url = `https://riderx.typeform.com/to/${translations[i18n.locale]
-        .common.typeform_id}`;
+      const url = `https://riderx.typeform.com/to/${
+        translations[i18n.locale].common.typeform_id
+      }`;
       return url;
     },
     dialowflow_url() {
-      const url = `https://app.talky.cards/?api=${translations[i18n.locale]
-        .common.dialogflow_id}`;
+      const url = `https://app.talky.cards/?api=${
+        translations[i18n.locale].common.dialogflow_id
+      }`;
       // console.log(url);
       return url;
     }
