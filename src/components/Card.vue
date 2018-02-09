d8201f31096e3eeef906f7219815cf6d3c1071b3
martin donadieu <martindonadieu@gmail.com>
2018-02-09 17:59:54 +0100

    fix lint eror

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -7,9 +7,9 @@
                 <div class="col-xl-9 mx-auto">
                   <img :src="twitterImg(usr().twitter)" class="user-icon"><br/>
                   <h2 class="mb-5">
-                    {{usr().firstName}} {{usr().lastName}}</br>
-                    {{usr().mobile}}</br>
-                    {{usr().email}}</br>
+                    {{usr().firstName}} {{usr().lastName}} <br/>
+                    {{usr().mobile}}<br/>
+                    {{usr().email}}<br/>
                   </h2>
                 </div>
                 <div class="col-md-10 col-lg-8 col-xl-7 mx-auto">
@@ -32,12 +32,12 @@
           <i class="fa fa-calendar-plus fa-4x fa-fw"></i>
           </a>
         </div>
-        </br> {{$t("card.meet")}}
+        <br/> {{$t("card.meet")}}
       </div>
       <div class="col-sm-3 col-6 text-center tile-left">
           <div>
             <a class="white-text" href="/">
-            <img src="../assets/logo.png" class="brand-logo-icon"></img>
+            <img src="../assets/logo.png" class="brand-logo-icon"/>
             </a>
           </div>
           {{$t("card.visit")}}
@@ -48,7 +48,7 @@
           <i class="fa fa-phone fa-4x fa-fw"></i>
           </a>
         </div>
-        </br>{{$t("card.call")}}
+        <br/>{{$t("card.call")}}
       </div>
       <div class="col-sm-3 col-6 text-center tile-right">
         <div >
@@ -56,7 +56,7 @@
           <i class="fa fa-envelope-open fa-4x fa-fw"></i>
           </a>
         </div>
-        </br>{{$t("card.email")}}
+        <br/>{{$t("card.email")}}
       </div>
     </div>
 
